b498f2627064694d7d4d5ba484ab70dbffc0c4bf
Abraham Ortiz <abraham.ortiz@leovegas.com>
2026-08-03 11:08:45 +0200

    chore(nvim): enable gitsigns current line blame

--- nvim/init.lua
+++ nvim/init.lua
@@ -187,6 +187,15 @@ do
       topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
       changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
     },
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+      use_focus = true,
+    },
   }
 
   vim.pack.add { gh 'folke/which-key.nvim' }

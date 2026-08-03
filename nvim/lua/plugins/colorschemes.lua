50306b0be1721f2a245187876599cfdf9d46cfc5
Abraham Ortiz <abraham.ortiz@leovegas.com>
2026-08-03 10:50:47 +0200

    Remove ghostty and colorschemes transparency

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -6,39 +6,8 @@ vim.pack.add {
   gh '/maxmx03/solarized.nvim',
 }
 
-require('ayu').setup {
-  overrides = function()
-    return {
-      Normal = { bg = 'None' },
-      NormalFloat = { bg = 'None' },
-      ColorColumn = { bg = 'None' },
-      SignColumn = { bg = 'None' },
-      Folded = { bg = 'None' },
-      FoldColumn = { bg = 'None' },
-      CursorLine = { bg = 'None' },
-      CursorColumn = { bg = 'None' },
-      VertSplit = { bg = 'None' },
-    }
-  end,
-}
-
-require('kanagawa').setup {
-  transparent = true,
-}
-
-require('solarized').setup {
-  transparent = {
-    enabled = true, -- Master switch to enable transparency
-    pmenu = true, -- Popup menu (e.g., autocomplete suggestions)
-    normal = true, -- Main editor window background
-    normalfloat = true, -- Floating windows
-    neotree = true, -- Neo-tree file explorer
-    nvimtree = true, -- Nvim-tree file explorer
-    whichkey = true, -- Which-key popup
-    telescope = true, -- Telescope fuzzy finder
-    lazy = true, -- Lazy plugin manager UI
-    mason = true, -- Mason manage external tooling
-  },
-}
+require('ayu').setup {}
+require('kanagawa').setup {}
+require('solarized').setup {}
 
 vim.cmd.colorscheme 'ayu-mirage'

5d80e81985ceead7777a96bc6cbae9fe9374eff7
Abraham Ortiz <abrahamortizu@gmail.com>
2026-08-08 00:07:55 +0200

    nvim: update

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,7 +1,7 @@
-vim.pack.add { 'http://github.com/folke/which-key.nvim' }
+vim.pack.add { 'https://github.com/folke/which-key.nvim' }
 
 require('which-key').setup {
-  delay = 0,
+  delay = 1000,
   preset = 'modern',
   icons = { mappings = vim.g.have_nerd_font },
   spec = {

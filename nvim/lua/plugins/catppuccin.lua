fb8eb6d7eff1cf3d1a01dcd6f68966229df90ef0
Abraham Ortiz <abraham.ortiz@leovegas.com>
2026-08-21 16:19:51 +0200

    nvim: change colorscheme to catppuccin

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
+
+require('catppuccin').setup {
+  auto_integrations = true,
+  transparent_background = true,
+  integrations = {
+    aerial = true,
+    diffview = true,
+    markview = true,
+    mason = true,
+  },
+}
+
+vim.cmd.colorscheme 'catppuccin-mocha'

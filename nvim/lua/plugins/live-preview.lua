2aad2f941bd663bce8658cf119058d1007d782d0
Abraham Ortiz <abraham.ortiz@leovegas.com>
2026-08-28 13:57:37 +0200

    nvim: add livepreview keymap

--- nvim/lua/plugins/live-preview.lua
+++ nvim/lua/plugins/live-preview.lua
@@ -1,3 +1,5 @@
 vim.pack.add { 'https://github.com/brianhuster/live-preview.nvim' }
 
-require('live-preview').setup {}
+vim.keymap.set('n', '<leader>tl', '<CMD>LivePreview start<CR>', { desc = 'Toggle `LivePreview`' })
+
+require('livepreview.config').set()

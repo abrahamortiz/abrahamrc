f8b4ce2b7a34bd1be4f0849adc84b7c12721479d
Abraham Ortiz <abrahamortizu@gmail.com>
2026-08-28 21:06:53 +0200

    nvim: remove todo-comments

--- nvim/lua/plugins/todo-comments.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add { 'https://github.com/folke/todo-comments.nvim' }
-
-require('todo-comments').setup { signs = false }
-
-vim.keymap.set('n', '<leader>st', '<CMD>TodoFzfLua<CR>', { desc = '[S]earch [T]odo comments' })

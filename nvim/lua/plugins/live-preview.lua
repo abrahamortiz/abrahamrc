e4ff9d28c828b38a7cde1998121e63ad2b8be9e5
Abraham Ortiz <abrahamortizu@gmail.com>
2026-09-01 21:13:46 +0200

    nvim: move livepreview keymap

--- nvim/lua/plugins/live-preview.lua
+++ nvim/lua/plugins/live-preview.lua
@@ -1,5 +1,5 @@
 vim.pack.add { 'https://github.com/brianhuster/live-preview.nvim' }
 
-vim.keymap.set('n', '<leader>tl', '<CMD>LivePreview start<CR>', { desc = 'Toggle `LivePreview`' })
-
 require('livepreview.config').set()
+
+vim.keymap.set('n', '<leader>tl', '<CMD>LivePreview start<CR>', { desc = 'Toggle `LivePreview`' })

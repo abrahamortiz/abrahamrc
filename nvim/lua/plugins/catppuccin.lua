b604a6b92064999ab5bafa78944c84f6e1a0168e
Abraham Ortiz <abraham.ortiz@leovegas.com>
2026-09-23 16:41:23 +0200

    style(nvim): update catppuccin config

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,9 +4,14 @@ require('catppuccin').setup {
   auto_integrations = true,
   transparent_background = true,
   integrations = {
+    fzf = true,
+    gitsigns = true,
     markview = true,
     mason = true,
+    mini = {
+      enabled = true,
+    },
   },
 }
 
-vim.cmd.colorscheme 'catppuccin-mocha'
+vim.cmd.colorscheme 'catppuccin-macchiato'
